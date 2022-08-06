e2a19bf9eddef8f51b34ae6ffe3ad27b06f9877c
Isaac Osuna <eddieisaac@gmail.com>
2022-08-06 17:52:21 -0600

    front-end base structure ready

--- stori-front/src/components/HelloWorld.vue
+++ stori-front/src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <ui-button icon="add" @click="$alert('olis')">Click Me!</ui-button>
     <p>
       For a guide and recipes on how to configure / customize this project,<br />
       check out the
--- stori-front/src/main.ts
+++ stori-front/src/main.ts
@@ -4,9 +4,14 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 
+import BalmUI from "balm-ui"; // Official Google Material Components
+
+import "balm-ui-css";
+
 const pinia = createPinia();
 const app = createApp(App);
 
 app.use(pinia);
+app.use(BalmUI);
 app.use(router);
 app.mount("#app");
